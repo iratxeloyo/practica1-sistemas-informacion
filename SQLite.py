eec8ec1819b862cbb13a7ad5f9c0388ed7ae72eb
iratxeloyo <iratxeloyo@gmail.com>
2022-03-26 12:28:31 +0100

    cambios

--- SQLite.py
+++ SQLite.py
@@ -23,18 +23,21 @@ def sql_delete_table(con):
     cursorObj.execute('drop table if exists usuarios')
     con.commit()
 
-def sql_create_table(con):
+def sql_create_tables(con):
     cursorObj = con.cursor()
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS usuarios (dni text, nombre text, altura real)")
-    cursorObj.execute("INSERT INTO usuarios VALUES ('X', 'isaac', '1.85') ")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS usuarios (telefono real, contrasena text, provincia text, permisos real, emails text, fechas text, ips text)")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS legal (cookies real, aviso real, proteccion_de_datos real, creacion real)")
+    #cursorObj.execute("INSERT INTO usuarios VALUES ('X', 'isaac', '1.85') ")
     con.commit()
 
-con = sqlite3.connect('example.db')
-sql_create_table(con)
-sql_fetch(con)
-sql_update(con)
-sql_fetch(con)
-sql_delete(con)
-sql_fetch(con)
-sql_delete_table(con)
+
+
+con = sqlite3.connect('database.db')
+sql_create_tables(con)
+#sql_fetch(con)
+#sql_update(con)
+#sql_fetch(con)
+#sql_delete(con)
+#sql_fetch(con)
+#sql_delete_table(con)
 con.close()
